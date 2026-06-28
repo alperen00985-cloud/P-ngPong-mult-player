8224d306c2bf6ad23615ac473cbd421f2828b711
Alperen <alperen00985@gmail.com>
2026-06-28 03:32:16 +0300

    Ponhg multiplayer oyun tatamini hersey okey

--- client.js
+++ client.js
@@ -19,8 +19,26 @@ let ballX = canvas.width / 2;
 let ballY = canvas.height / 2;
 let ballSpeedX = 0.7;
 let ballSpeedY = 0.7;
-let score1 = 0
-let score2 = 0
+let score1 = 0;
+let score2 = 0;
+let player1Name = "";
+let player2Name = "";
+let playerName = "";
+let gameStart = false;
+
+//Isim alma
+document.getElementById("joinButon").onclick = () =>{
+    playerName = document.getElementById("nameInput").value.trim();
+    if (playerName === ""){
+        alert("Isim giriniz!");
+        return;
+    }
+    socket.send(JSON.stringify({
+        type: "join",
+        name: playerName
+    }));
+    document.getElementById("menu").style.display = "none";
+}
 
 
 
@@ -30,22 +48,43 @@ function draw() {
     ctx.fillStyle = "black";
 ctx.fillRect(0,0,canvas.width,canvas.height);
  
-ctx.fillStyle = "red";
+ctx.fillStyle = "#fd1c03";
     ctx.fillRect(x1, y1, 20,100 );
-ctx.fillStyle = "red";
+ctx.fillStyle = "#FD1C03";
     ctx.fillRect(x2, y2, 20, 100);
 
-    ctx.fillStyle = "orange" ;
+    
+
+    ctx.fillStyle = "#FF6600" ;
     ctx.font = "30px Arial" ;
     ctx.textAlign = "center" ;
     ctx.fillText(`${score1} | ${score2}`, canvas.width / 2 , 43)
-    
 
+    ctx.fillStyle = "#00aeff"
+    ctx.font = "20px Arial"
+    ctx.textAlign = "right"
+    ctx.fillText(player1Name,canvas.width / 2 - 60 , 38)
+    ctx.textAlign = "left"
+    ctx.fillText(player2Name,canvas.width / 2 + 60 , 38)
+
+    if (ballX && ballY) {
     ctx.beginPath();
-    ctx.arc(ballX, ballY , 20, 0, Math.PI * 2);
-    ctx.fillStyle = "gray";
+    ctx.arc(ballX, ballY, 20, 0, Math.PI * 2);
+    ctx.fillStyle = "#f507b9";
     ctx.fill();
     ctx.closePath();
+
+}
+if (!gameStart) {
+    ctx.fillStyle = "#ffee00";
+    ctx.font = "45px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText(
+        "Oyuncular bekleniyor...",
+        canvas.width / 2,
+        canvas.height / 2 - 50
+    );
+}
     
 }
 // klavye tuslariyla paddle hareketi
@@ -73,13 +112,15 @@ socket.onmessage = (event) => {
     console.log("STATE:", data);
 
     if (data.type === "state") {
-        y1 = data.player1Y;
-        y2 = data.player2Y;
-
-        ballX = data.ball.x;
-        ballY = data.ball.y;
+    y1 = data.player1Y;
+    y2 = data.player2Y;
+    ballX = data.ball.x;
+    ballY = data.ball.y;
     score1 = data.score1;
     score2 = data.score2;
+    player1Name = data.player1Name ?? "";
+    player2Name = data.player2Name ?? "";
+    gameStart = data.gameStart ?? false;
     
     }
 };
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-;
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -9,72 +8,78 @@ const wss = new WebSocket.Server({ server });
 
 app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+server.listen(3000, () => {
+    console.log("Server 3000 portunda çalışıyor");
 });
 
-server.listen(3000);
+// Oyun nesneleri
 
-;
+const CANVAS_H = 500;
+const PADDLE_H = 100;
 
- const CANVAS_H = 500;
- const PADDLE_H = 100;
 let player1Y = 100;
 let player2Y = 100;
-let players = {}
-let playerCount = 0
+
+let slots = {
+    oyuncu1:null,
+    oyuncu2:null,
+    izleyici1:null,
+    izleyici2:null
+}
+
 let ball = {
     x: 400,
     y: 250,
     vx: 3.5,
     vy: 3.5
 };
-let score1 = 0
-let score2 = 0
 
-wss.on("connection", (ws) => {
-     playerCount++ ;
-     let playerID;
-     if(playerCount===1) playerID = "oyuncu1"    
-     if(playerCount===2) playerID = "oyuncu2" 
-     
-     players[playerID] = ws;
+let score1 = 0;
+let score2 = 0;
+let player1Name = "";
+let player2Name = "";
+let gameStart = false;
 
-     console.log(playerID + " baglandi");
+// Topun bastan baslamasi
 
-    ws.on("message", (msg) => {
-        console.log("Gelen:", msg.toString());
-
-        const data = JSON.parse(msg);
-
-        if (playerID === "oyuncu1") {
-            player1Y += data.direction * 15;
-
-            player1Y = Math.max(0, Math.min(CANVAS_H - PADDLE_H, player1Y));
-        }
-        if (playerID === "oyuncu2"){
-            player2Y += data.direction * 15 ;
-            player2Y = Math.max(0, Math.min(CANVAS_H - PADDLE_H, player2Y)) 
-        }
-       });
-       function resetBall() {
+function resetBall() {
     ball.x = 400;
     ball.y = 250;
     ball.vx = 3.5 * (Math.random() > 0.5 ? 1 : -1);
     ball.vy = 3.5 * (Math.random() > 0.5 ? 1 : -1);
 }
 
-    const interval = setInterval(() => {
+// Oyun hareketleri
+
+setInterval(() => {
+    if (gameStart) {
+    
+
+
     ball.x += ball.vx;
     ball.y += ball.vy;
 
-    if (ball.y <= 20 || ball.y >= 480) ball.vy *= -1;
+    if (ball.y <= 0 || ball.y >= 480) {
+        ball.vy *= -1;
+    }
 
-    if (ball.x <= 40 && ball.y >= player1Y && ball.y <= player1Y + PADDLE_H)
+    if (
+        ball.x <= 40 &&
+        ball.y >= player1Y &&
+        ball.y <= player1Y + PADDLE_H
+    ) {
+        ball.x = 40;
         ball.vx *= -1;
+    }
 
-    if (ball.x >= 740 && ball.y >= player2Y && ball.y <= player2Y + PADDLE_H)
+    if (
+        ball.x >= 740 &&
+        ball.y >= player2Y &&
+        ball.y <= player2Y + PADDLE_H
+    ) {
+        ball.x = 740;
         ball.vx *= -1;
+    }
 
     if (ball.x < 0) {
         score2++;
@@ -84,30 +89,83 @@ wss.on("connection", (ws) => {
     if (ball.x > 800) {
         score1++;
         resetBall();
-    }
+    }}
 
-    Object.values(players).forEach(client => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(JSON.stringify({
-                type: "state",
-                player1Y,
-                player2Y,
-                ball,
-                score1,
-                score2
-            }));
-        }
+    // Verilerin cliente yollanmasi
+    const state = JSON.stringify({
+        type: "state",
+        player1Y,
+        player2Y,
+        ball,
+        score1,
+        score2,
+        player1Name,
+        player2Name,
+        gameStart
+    });
+
+    Object.values(slots).forEach(client => {
+        if (client && client.readyState === WebSocket.OPEN) {
+        client.send(state);
+    }
     });
 
 }, 16);
 
+// Baglanti
 
-    
+wss.on("connection", (ws) => {
 
+    let playerID = null;
+
+if (!slots.oyuncu1) playerID = "oyuncu1";
+else if (!slots.oyuncu2) playerID = "oyuncu2";
+else if (!slots.izleyici1) playerID = "izleyici1";
+else if (!slots.izleyici2) playerID = "izleyici2";
+else {
+    ws.close();
+    return;
+}
+
+    slots[playerID] = ws;
+    ws.playerID = playerID;
+
+    console.log(playerID + " bağlandı");
+
+    ws.on("message", (msg) => {
+
+        const data = JSON.parse(msg);
+        if (data.type === "join") {
+            
+
+   
+     if (ws.playerID === "oyuncu1") player1Name = data.name;
+    if (ws.playerID === "oyuncu2") player2Name = data.name;
+
+    if (player1Name && player2Name) {
+        gameStart = true;
+        console.log("Oyun başladı");
+    }
+
+    return;
+}
+
+        if (ws.playerID === "oyuncu1") {
+            player1Y += data.direction * 15;
+            player1Y = Math.max(0, Math.min(CANVAS_H - PADDLE_H, player1Y));
+        }
+
+        if (ws.playerID === "oyuncu2") {
+            player2Y += data.direction * 15;
+            player2Y = Math.max(0, Math.min(CANVAS_H - PADDLE_H, player2Y));
+        }
+    });
 
     ws.on("close", () => {
-        clearInterval(interval);
+         if (slots[ws.playerID] === ws) {
+        slots[ws.playerID] = null;
+    }
+
+    console.log(ws.playerID + " ayrıldı");
     });
 });
-
-console.log("WebSocket server 3000 portunda çalışıyor");
